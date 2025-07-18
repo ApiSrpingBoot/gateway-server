d1376600886af1ecbf8b4f92837fa765dd3a3a60
luisl <llima@planerp.ec>
2025-07-18 12:57:52 -0500

    add: Servicio que actúa como puerta de entrada para todos los microservicios del sistema

---
+++ src/test/java/ec/llima/springcloud/ms/gateway/gateway_server/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.llima.springcloud.ms.gateway.gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
